14ec46b1c6d662a8600427b392f074bca2f15fef
vikas-dunitech <vikasdunitech@gmail.com>
2025-08-04 13:18:17 +0530

    Ralksdjfs

--- android/app/src/main/java/com/social/MainApplication.kt
+++ android/app/src/main/java/com/social/MainApplication.kt
@@ -24,7 +24,7 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
-              // add(VectorIconsPackage())
+             
                add(LinearGradientPackage())
             }
 
